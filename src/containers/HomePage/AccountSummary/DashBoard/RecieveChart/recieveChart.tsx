9597ed0d1fba22ddc320e0104927219284dd9397
Hong Zhang <sheetstone@gmail.com>
2023-06-03 15:36:27 -0500

    update recieveChart

--- src/containers/HomePage/AccountSummary/DashBoard/RecieveChart/recieveChart.tsx
+++ src/containers/HomePage/AccountSummary/DashBoard/RecieveChart/recieveChart.tsx
@@ -2,7 +2,7 @@ import Chart from "react-google-charts";
 import PropTypes from "prop-types";
 
 import classes from "./recieveChart.module.scss";
-import { PaymentInterface } from "api/payment";
+import { PaymentInterface, PaymentType } from "api/payment";
 
 // with google-charts;
 const options = {
@@ -54,35 +54,51 @@ const RecieveChart = (props: {
     }
   };
 
-  const getChartDataByKey = (
+  /**
+   * Returns an array of key-value pairs for the given key and payment type.
+   * @param key - The key to group the payment data by.
+   * @param type - The type of payment to filter by.
+   * @returns An array of key-value pairs representing the payment data grouped by the given key.
+   * @example
+   * [
+   *  ['bank', 'Amount']
+   *  ['Bank A', 1000],
+   *  ['Bank B', 2000],
+   *  ['Bank C', 1500],
+   *  ['Bank D', 500],
+   * ]
+   */
+  function getChartDataByKey(
     key: "name" | "bank" | "casenumber" | "catgory",
-    sta: number
-  ) => {
-    const keylist: string[] = [];
-    const fineddata: any[] = [[key, "Amount"]];
+    type: PaymentType
+  ): [string, string | number][] {
+    const summaryMap = new Map<string, number>();
+    const tableHeader: [string, string | number][] = [[key, "Amount"]];
 
-    if (paymentData.length === 0) return 0;
-    //console.log(paymentData);
-    paymentData.forEach((payment) => {
-      if (
-        payment[key] !== undefined &&
-        !keylist.includes(payment[key] as string)
-      ) {
-        keylist.push(payment[key] as string);
-      }
-    });
-    const group = keylist.map((item) => {
-      let sum = 0;
-      paymentData.forEach((piece) => {
-        if (piece[key] === item && piece.type === sta) {
-          sum += (typeof piece.amount == 'string') ? parseFloat(piece.amount): piece.amount;
+    if (paymentData.length === 0) return [];
+    for (const payment of paymentData) {
+      if (payment[key] !== undefined && payment.type === type) {
+        const keyName = payment[key] as string;
+        if (summaryMap.has(keyName)) {
+          summaryMap.set(
+            keyName,
+            summaryMap.get(keyName)! +
+              (typeof payment.amount === "string"
+                ? parseFloat(payment.amount)
+                : payment.amount)
+          );
+        } else {
+          summaryMap.set(
+            keyName,
+            typeof payment.amount === "string"
+              ? parseFloat(payment.amount)
+              : payment.amount
+          );
         }
-      });  
-      return [item, sum];
-    });
-    console.log(group)
-    return fineddata.concat(group);
-  };
+      }
+    }
+    return tableHeader.concat(Array.from(summaryMap.entries()));
+  }
 
   return (
     <div>
