573663413e83e1d98feaa32b15fbe7235d15be69
Hong Zhang <sheetstone@gmail.com>
2024-02-24 22:57:07 -0600

    update

--- src/containers/Login/login.tsx
+++ src/containers/Login/login.tsx
@@ -4,28 +4,32 @@ import LoginContainer from "./loginContainer";
 export default function Login() {
   return (
     <>
-      <div className="container mx-auto">
-        <div className="gap-8 columns-2">
-          <div className={classes.textContainer+" w-full relative"}>
-            <div className={classes.textOverlay}>
-              <h2>Welcome</h2>
-              <div>
-                <p>
-                  ExpertPay allows employers to electronically submit child
-                  support payments that they are required to withhold from
-                  employees' paychecks.
-                </p>
-                <p className="text-3xl font-bold underline">
-                  With your ExpertPay<sup>SM</sup> account you can save and
-                  maintain employee payment information for quick transactions.
-                  You can also add multiple users to the account, individually
-                  defining each user's authorization.
-                </p>
+      <div className={classes.heroBanner}>
+        <div className="container mx-auto">
+          <div className="gap-8 columns-2 flex items-center justify-center">
+            <div className={classes.textContainer}>
+              <div className={classes.textOverlay}>
+                <div>
+                  <p className={classes.paper+" "+classes.stickyNotes1}>
+                    ExpertPay allows employers to electronically submit child
+                    support payments that they are required to withhold from
+                    employees' paychecks.
+                  </p>
+                  <p className={classes.paper+" "+classes.stickyNotes2}>
+                    With your ExpertPay<sup>SM</sup> account you can save and
+                    maintain employee payment information for quick
+                    transactions. 
+                  </p>
+                  <p className={classes.paper+" "+classes.stickyNotes3}>
+                  You can also add multiple users to the
+                    account, individually defining each user's authorization.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-full">
-            <LoginContainer />
+            <div className="w-full">
+              <LoginContainer />
+            </div>
           </div>
         </div>
       </div>
--- src/containers/Login/loginContainer/index.tsx
+++ src/containers/Login/loginContainer/index.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "../../../api/auth/auth.store";
 import { useNavigate } from "react-router-dom";
 import classes from "./index.module.scss";
+import GoogleIcon from "../../../assets/images/googleIcon.svg"; 
 
 export default function LoginContainer() {
   const { handleSignInWithGoogle, handleSignOut, isLogin, userData } =
@@ -17,12 +18,13 @@ export default function LoginContainer() {
         <h1 className={classes.login}>Login</h1>
 
         <div className="px-8 py-2">
-          <h2>Log in to My ExpertPay to continue</h2>
+          <h2 className="text-2xl">Welcome back</h2>
           <button
-            className="bg-white text-grey p-2 rounded-md w-full flex items-center border-"
+            className={classes.googleBtn+" rounded-md w-full flex items-center border border-gray-300 mb-4 mt-3"}
             onClick={handleSignInWithGoogle}
           >
-            Login with Google
+            <img src={GoogleIcon} alt="google" className="w-6 h-6 mr-2" />
+            Sign in Google
           </button>
 
           <div className="flex items-center mb-4">
@@ -37,15 +39,15 @@ export default function LoginContainer() {
               className="mt-1 p-2 w-full border rounded-md"
             />
           </div>
-          <div>
+          <div className="mb-2">
             <input
               type="password"
               placeholder="Password"
-              className="mt-1 p-2 w-full border rounded-md"
+              className="mt-2 p-2 w-full border rounded-md"
             />
           </div>
-          <a href="#">Forgot your password?</a>
-          <div>
+          <a href="#" >Forgot your password?</a>
+          <div className="mt-2">
             <button
               onClick={() => null}
               className="rounded-md w-full mb-4 btn btn-primary"
